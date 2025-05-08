fbdc5f1c4668b97533fdef6b010ba8236e50c467
LSWasTaken <120115418+LSWasTaken@users.noreply.github.com>
2025-05-08 23:01:19 +0800

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDpawZZghEI9eAlx7XEx8luR-UaPxwm2Vw",
-    authDomain: "tanza-fighters.firebaseapp.com",
-    projectId: "tanza-fighters",
-    storageBucket: "tanza-fighters.firebasestorage.app",
-    messagingSenderId: "537721612057",
-    appId: "1:537721612057:web:6d5c94cf1cdda34a2e4788"
+  apiKey: "AIzaSyDpawZZghEI9eAlx7XEx8luR-UaPxwm2Vw",
+  authDomain: "tanza-fighters.firebaseapp.com",
+  projectId: "tanza-fighters",
+  storageBucket: "tanza-fighters.firebasestorage.app",
+  messagingSenderId: "537721612057",
+  appId: "1:537721612057:web:6d5c94cf1cdda34a2e4788"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
